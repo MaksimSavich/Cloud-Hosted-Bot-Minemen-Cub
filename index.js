075acc131216a0d91cba17f1626edd15e8ed0b55
Maksim Savich <mpsavich@icloud.com>
2019-07-06 13:08:14 -0400

    1.0.1

---
+++ index.js
@@ -0,0 +1,60 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity('Minemen Den | ^help', { type: 'WATCHING' });
+})
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+
+
+if(cmd === `${prefix}hello`){
+  return message.channel.send("Hello!");
+}
+
+if(cmd === `${prefix}serverinfo`){
+
+  let sicon = message.guild.displayAvatarUrl;
+  let serverembed = new Discord.RichEmbed()
+  .setTitle("__Server Information__")
+  .setColor("#af7ac5")
+  .setThumbnail(sicon)
+  .addField("Server Name", message.guild.name)
+  .addField("Created On", message.guild.createdAt)
+  .addField("You Joined", message.member.joinedAt)
+  .addField("Toatal Members", message.guild.memberCount);
+
+  return message.channel.send(serverembed);
+}
+
+if(cmd === `${prefix}help`){
+
+  let bicon = bot.user.displayAvatarUrl
+  let botembed = new Discord.RichEmbed()
+  .setTitle("__Bot Commands__")
+  .setColor("#af7ac5")
+  .setThumbnail(bicon)
+  .addField("Command List", display = "^help | ^serverinfo");
+
+
+  return message.channel.send(botembed);
+}
+
+
+
+});
+
+
+
+bot.login(botconfig.token);
