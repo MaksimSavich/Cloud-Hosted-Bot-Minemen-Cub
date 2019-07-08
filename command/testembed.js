80dbb6ef158d0af816a181fd04ebb1868a653776
Maksim Savich <mpsavich@icloud.com>
2019-07-08 15:04:41 -0400

    1.2.0

--- command/testembed.js
+++ command/testembed.js
@@ -3,13 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let bicon = bot.user.displayiconUrl;
-  let timestamp = date.now();
+  let footurl = message.author.avatarURL;
+  let timestamp = date.now(timestamp);
   let botembed = new Discord.RichEmbed()
   .setTitle("__Bot Commands__")
   .setColor("#af7ac5")
   .setThumbnail(bicon)
   .addField("Command List", display = "^help | ^serverinfo | ^report | ^help staff")
-  .setTimestamp(timestamp);
+  .setfooter(footurl)
 
   return message.channel.send(botembed);
 }
