8331d820040e937b703defa9dcad14ba26052e6f
Maksim Savich <mpsavich@icloud.com>
2019-07-08 16:41:07 -0400

    1.2.0

--- command/help.js
+++ command/help.js
@@ -1,15 +1,31 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+ 
+  message.channel.send(`<@${message.author.id}> you have been privately messaged the help commands.`);
 
-  let botembed = new Discord.RichEmbed()
-  .setTitle("__Bot Commands__")
-  .setColor("#af7ac5")
-  .setThumbnail(bot.message.avatarURL)
-  .addField("Command List", display = "^help | ^serverinfo | ^report | ^help staff");
+  message.member.send({embed: {
+    color: 6000,
+    author: {
+      name: "Minemen Cub | Help", 
+      icon_url: bot.user.avatarURL,
+    },
+    title: "Commands",
+    description: "^help | ^serverinfo | ^report",
+    fields: [{
+        name: "Report",
+        value: "Example: ^report {User} {Reason}."
+      },
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: message.author.avatarURL,
+      text: `<@${message.author.id}>`
+    }
+  }
+});
 
-
-  return message.channel.send(botembed);
+  message.delete().catch(O_o=>{});
 }
 
 module.exports.help = {
