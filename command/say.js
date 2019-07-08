5fc447c51989862b03d9d665cc6b51afa0457245
Maksim Savich <mpsavich@icloud.com>
2019-07-08 14:38:40 -0400

    1.2.0

--- command/say.js
+++ command/say.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
   let sayRole = message.guild.roles.find("name", "• say •");
   if(!message.member.roles.has(sayRole.id)) return message.member.send("You can't perform this action!");
+  if(!message.member.roles.has(sayRole.id)) return message.delete().catch(O_o=>{});
   if(message.member.roles.has(sayRole.id)){
       let botmessage = args.join(" ");
       message.delete().catch();
