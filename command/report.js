2043d399f67bda1f1e2226475aa76e33fe86ac82
Maksim Savich <mpsavich@icloud.com>
2019-07-08 13:45:05 -0400

    1.1.0

--- command/report.js
+++ command/report.js
@@ -1,7 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (vor, message, args) => {
-  console.log("works")
+
+  let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+  if(!rUser) return message.channel.send("Couldn't find user.");
+  let reason = args.join(" ").slice(22);
+
+  let reportEmbed = new Discord.RichEmbed()
+  .setDescription("Reports")
+  .setColor("#af7ac5")
+  .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+  .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+  .addField("Channel", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Reason", reason);
+
+  let reportschannel = message.guild.channels.find(`name`, "reports");
+  if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+  message.delete().catch(O_o=>{});
+  reportschannel.send(reportEmbed);
 }
 
 module.exports.help = {
