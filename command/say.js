2f5305d9d402bd3f195c1c5b6248089cf25474f6
Maksim Savich <mpsavich@icloud.com>
2019-07-08 14:32:51 -0400

    1.2.0

--- command/say.js
+++ command/say.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let sayRole = message.guild.roles.find("name", "• say •");
-  if(!message.member.roles.has(sayRole.id)) return return message.member.send("You can't perform this action!");
+  if(!message.member.roles.has(sayRole.id)) return message.member.send("You can't perform this action!");
   if(message.member.roles.has(sayRole.id)){
       let bot message = args.join(" ");
       message.delete().catch();
