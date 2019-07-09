a1ff0c2e96b79b268db535963cae89c0f1a42e5f
Maksim Savich <mpsavich@icloud.com>
2019-07-09 16:37:08 -0400

    1.2.4

--- command/testannouncement.js
+++ command/testannouncement.js
@@ -1,49 +1,49 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-
-  let split = `%%`
-
-  if (!args[0]) {
-
-      const embed = new Discord.MessageEmbed()
-        .setColor("#af7ac5")
-        .setTitle(`Information`)
-        .setDescription(`**Usage: \`\`\`!announce embedTitle ${split} embedMsg ${split} msgName ${split} msgIcon ${split} embedColor\`\`\`**`);
-
-        return send(message.channel, embed, {
-          name: `Announce Command`,
-          icon: bot.user.displayAvatarURL
-
-        })
-  }
-
-  args =args.join(` `).split(split);
-
-  for (var i =0; i<args.length; i++) args[i] = args[i].trim();
-
-  if(args[4]) args[4] = parseInt(`0x${args[4]}`)
-
-  let options = {
-    title: args[0] || `Announcement`,
-    message: args[1] || undefined,
-    name: args [2] || `Minemen Den`,
-    icon: args[3] || bot.user.displayAvatarURL,
-    embedColor: args[4] || 0xffffff
-  }
-
-  const embed = new Discord.MessageEmbed()
-    .setColor(options.embedColor)
-    .setTitle(options.title)
-
-    if(options.message) embed.setDescription(options.message);
-
-    send(message.channel, embed, {
-      name: options.name,
-      icon: options.icon
-    })
-}
-
-module.exports.help = {
-  name: "ta"
-}
+// const Discord = require("discord.js");
+//
+// module.exports.run = async (bot, message, args) => {
+//
+//   let split = `%%`
+//
+//   if (!args[0]) {
+//
+//       const embed = new Discord.MessageEmbed()
+//         .setColor("#af7ac5")
+//         .setTitle(`Information`)
+//         .setDescription(`**Usage: \`\`\`!announce embedTitle ${split} embedMsg ${split} msgName ${split} msgIcon ${split} embedColor\`\`\`**`);
+//
+//         return send(message.channel, embed, {
+//           name: `Announce Command`,
+//           icon: bot.user.displayAvatarURL
+//
+//         })
+//   }
+//
+//   args =args.join(` `).split(split);
+//
+//   for (var i =0; i<args.length; i++) args[i] = args[i].trim();
+//
+//   if(args[4]) args[4] = parseInt(`0x${args[4]}`)
+//
+//   let options = {
+//     title: args[0] || `Announcement`,
+//     message: args[1] || undefined,
+//     name: args [2] || `Minemen Den`,
+//     icon: args[3] || bot.user.displayAvatarURL,
+//     embedColor: args[4] || 0xffffff
+//   }
+//
+//   const embed = new Discord.MessageEmbed()
+//     .setColor(options.embedColor)
+//     .setTitle(options.title)
+//
+//     if(options.message) embed.setDescription(options.message);
+//
+//     send(message.channel, embed, {
+//       name: options.name,
+//       icon: options.icon
+//     })
+// }
+//
+// module.exports.help = {
+//   name: "ta"
+// }
