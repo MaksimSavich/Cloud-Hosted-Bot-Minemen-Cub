d6805da4a3e40af654bb881a7c2c261cf436d515
Maksim Savich <mpsavich@icloud.com>
2019-07-09 17:09:09 -0400

    1.2.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const client = new Discord.client({disableEveryone: true});
+const client = new Discord.client();
 
 
 
