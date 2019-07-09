8e1ab40112269246e85aaa55fba867d715181dfd
Maksim Savich <mpsavich@icloud.com>
2019-07-09 01:07:10 -0400

    1.2.1

--- command/announce.js
+++ command/announce.js
@@ -3,14 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let usermessage = args.join(" ")
-
+  let userrmessage = args.join("-")
 
   let userembed = new Discord.RichEmbed()
 
   .setColor("#af7ac5")
   .setFooter(`<@${message.author.id}>`)
   .setTimestamp()
-  .addField(usermessage, usermessage);
+  .addField(usermessage, userrmessage);
 
   return message.channel.send(userembed);
 
