9c0a1c55386307b633bd5f2df97da6faa6056275
Maksim Savich <mpsavich@icloud.com>
2019-07-08 22:58:07 -0400

    1.2.1

--- command/testembed.js
+++ command/testembed.js
@@ -4,9 +4,12 @@ module.exports.run = async (bot, message, args) => {
 
 
 
-  let botembed = new Discord.RichEmbed()
+  let userembed = new Discord.RichEmbed()
+  let botmessage = args.join(" ");
+  message.delete().catch();
+  message.channel.send(botmessage);
 
-  return message.channel.send(botembed);
+  return message.channel.send(userembed);
 }
 
 module.exports.help = {
