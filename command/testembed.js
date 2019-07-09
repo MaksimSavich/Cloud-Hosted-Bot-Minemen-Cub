6b957f96b4ac1ab4d2d2f741730bba89432f3d67
Maksim Savich <mpsavich@icloud.com>
2019-07-08 23:22:35 -0400

    1.2.1

--- command/testembed.js
+++ command/testembed.js
@@ -3,9 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 
+  let botmessage = args.join(" ")
 
   let userembed = new Discord.RichEmbed()
-  .setDescription(message.author.send)
+  .setDescription(`${message.channel.send(botmessage)}`)
   .setColor("#af7ac5");
 
   return message.channel.send(userembed);
