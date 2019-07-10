2af1a5abaabaf844140d6cbd7203d01d007c16e4
Maksim Savich <mpsavich@icloud.com>
2019-07-09 21:45:13 -0400

    1.2.5

--- command/a.js
+++ command/a.js
@@ -1,7 +1,9 @@
+const Discord = require(`discord.js`);
+
 exports.run = async (client, message, args, tools) => {
 
   let sayRole = message.guild.roles.find("name", "• say •");
-    
+
 
     if(message.member.roles.has(sayRole.id)){
 
