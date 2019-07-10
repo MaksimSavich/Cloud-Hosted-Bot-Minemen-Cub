27c69e7049fa7308d3ac0835aaae0b09afd97978
Maksim Savich <mpsavich@icloud.com>
2019-07-09 21:13:39 -0400

    1.2.5

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ client.on("message", async message => {
 
   let commandfile = client.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(client, message, args);
+}
 
 
 client.on("ready", async () => {
