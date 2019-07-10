994752dc2296f888865e37c5b6d1a175f14277c7
Maksim Savich <mpsavich@icloud.com>
2019-07-10 16:35:56 -0400

    1.2.5

--- command/a.js
+++ command/a.js
@@ -1,21 +1,11 @@
 const Discord = require(`discord.js`);
 
-exports.run = async (client, message, args, tools) => {
+exports.run = async (client, message, args) => {
 
-  let sayRole = message.guild.roles.find("name", "• say •");
-
-
-    if(message.member.roles.has(sayRole.id)){
-
-      let usermessage = args.join(" ")
 
   let userembed = new Discord.RichEmbed()
 
   .setColor("#af7ac5")
-  .setFooter(message.author.displayAvatarURL, `${message.author.tag}`)
-  .setTimestamp()
-  .addField("**Announcement**", usermessage);
-
 
   return message.channel.send(userembed)
 
