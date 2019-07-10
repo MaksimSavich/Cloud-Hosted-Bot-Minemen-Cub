27d3423f421293fba6de80026a82091ca19a9fad
Maksim Savich <mpsavich@icloud.com>
2019-07-09 20:26:22 -0400

    1.2.5

--- command/ta.js
+++ command/ta.js
@@ -1,7 +1,7 @@
 const send = require(`quick.hook`);
 const Discord = require("discord.js");
 
-exports.run = async .catch(client, message, args, tools) => {
+exports.run = async.catch(client, message, args, tools) => {
 
   let split = `&`
 
