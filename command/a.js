7cab9971e2b10ffcb358897970d394f3aa1ec6fa
Maksim Savich <mpsavich@icloud.com>
2019-07-09 21:35:06 -0400

    1.2.5

--- command/a.js
+++ command/a.js
@@ -4,12 +4,13 @@ exports.run = async (client, message, args, tools) => {
     if(!message.member.roles.has(sayRole.id)) return message.delete().catch(O_o=>{}), message.member.send("You can't perform this action!");
     let sayRole = message.guild.roles.find("name", "• say •");
     if(message.member.roles.has(sayRole.id)){
-  let usermessage = args.join(" ")
+ 
+      let usermessage = args.join(" ")
 
   let userembed = new Discord.RichEmbed()
 
   .setColor("#af7ac5")
-  .setFooter(message.author.displayAvatarURL, `${message.author.tag}`)
+  .setFooter()
   .setTimestamp()
   .addField("**Announcement**", usermessage);
     }
