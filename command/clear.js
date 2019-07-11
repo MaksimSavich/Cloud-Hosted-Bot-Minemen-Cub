0c577c75103e355b204795a63f32003ab96ec13c
Maksim Savich <mpsavich@icloud.com>
2019-07-11 17:22:44 -0400

    1.2.6

--- command/clear.js
+++ command/clear.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args, tools) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't perform this action!");
   if(!args[0]) return message.channel.send("You can't perform this action!");
@@ -8,8 +8,4 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(`Cleared ${args[0]} messages.`).then (msg => msg.delete(1500));
   })
 
-}
-
-module.exports.help = {
-  name: "clear"
 }
