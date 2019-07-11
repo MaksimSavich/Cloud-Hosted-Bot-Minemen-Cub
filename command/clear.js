1e970941fd9c8bdd6c60d1c4848bc64787cf9b17
Maksim Savich <mpsavich@icloud.com>
2019-07-11 17:23:55 -0400

    1.2.6

--- command/clear.js
+++ command/clear.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args, tools) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't perform this action!");
   if(!args[0]) return message.channel.send("You can't perform this action!");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then (msg => msg.delete(1500));
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(1500));
   });
 
 }
