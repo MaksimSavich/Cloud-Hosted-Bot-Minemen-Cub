7d5f4b7a3207a5ebaa55df82f2a08f17dde1f657
Maksim Savich <mpsavich@icloud.com>
2019-07-12 01:42:48 -0400

    1.2.7

--- command/addrole.js
+++ command/addrole.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args, tools) => {
 
   let roleRole = message.guild.roles.find("name", "• role •");
-      if(!message.member.roles.has(roleRole.id)) return message.member.send(`You can't perform this action!`), (message.delete().catch(O_o=>{}));
+      if(!message.member.roles.has(roleRole.id)) return message.member.reply(`You can't perform this action!`), (message.delete().catch(O_o=>{}););
       if(message.member.roles.has(roleRole.id)){
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-      if(!rMember) return message.member.send("Can't find user!");
+      if(!rMember) return message.member.send(1Can't find user!");
   let role = args.join(" ").slice(22);
       if(!role) return message.member.send(`Specify a role!`);
   let gRole = message.guild.roles.find(`name`, role);
@@ -18,7 +18,7 @@ exports.run = async (client, message, args, tools) => {
         try{
           await rMember.send(`You have been awarded the role ${gRole}!`)
         }catch(e){
-          message.channel.send(`<@${rMember.id}> you have been awarded the role ${gRole}! We tried to DM you but they were locked.`)
+          message.channel.sendrMember.send(`<@${rMember.id}> you have been awarded the role ${gRole}! We tried to DM you but they were locked.`)
     }
       message.delete().catch(O_o=>{});
   }
