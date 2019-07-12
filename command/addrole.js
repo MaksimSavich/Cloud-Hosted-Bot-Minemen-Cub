9abb87928fe0718ea51336ded31bb319a456ef92
Maksim Savich <mpsavich@icloud.com>
2019-07-12 00:51:40 -0400

    1.2.7

--- command/addrole.js
+++ command/addrole.js
@@ -3,7 +3,7 @@ Const Discord = require("discord.js");
 exports.run = async (client, message, args, tools) => {
 
   let roleRole = message.guild.roles.find("name", "• role •");
-      if(!message.member.roles.has(roleRole.id)) return message.member.reply(`You can't perform this action!`);
+      if(!message.member.roles.has(roleRole.id)) return message.member.send(`You can't perform this action!`);
       if(message.member.roles.has(roleRole.id)){
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
       if(!rMember) return message.member.send(1Can't find user!");
@@ -21,4 +21,5 @@ exports.run = async (client, message, args, tools) => {
           message.channel.sendrMember.send(`<@${rMember.id}> you have been awarded the role ${gRole}! We tried to DM you but they were locked.`)
     }
   }
+  message.delete().catch(O_o=>{});
 }
