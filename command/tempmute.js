c650e97b73fc5f90a6ad93dcbbe64468b2d0fc7c
Maksim Savich <mpsavich@icloud.com>
2019-07-20 21:19:34 -0400

    1.3.4

--- command/tempmute.js
+++ command/tempmute.js
@@ -32,7 +32,7 @@ const embed = new Discord.RichEmbed()
      .setColor(`#af7ac5`)
      .setThumbnail(message.guild.member(message.mentions.users.first()).avatarURL)
      .setTimestamp()
-     .setAuthor("Punished User.", `${tomute.id}.avatarURL`)
+     .setAuthor("Punished User.", `${tomute.id}.iconURL`)
      .addField((`<@${tomute.id}>`), "Roles: " + message.guild.member(message.mentions.users.first()).roles.map(role => role.name).join(", ")); // user, roles
      
      let roleChannel = message.guild.channels.find(`name` , `punished-players-roles`);
