a941a150c93aa4c27caac7dd144818e826d9ec4b
Maksim Savich <mpsavich@icloud.com>
2019-07-23 19:41:49 -0400

    1.3.4

--- command/tempmute.js
+++ command/tempmute.js
@@ -52,8 +52,7 @@ if(!muterole){
           muteChannel.send(muteEmbed);
 
   {
-          tomute.removeRoles(tomute.roles);
-          tomute.addRole(muterole.id)
+          (tomute.removeRoles(tomute.roles)) , (tomute.addRole(muterole.id))
 
       try{
         await tomute.send(`You have been muted for ${mutetime}!`)
@@ -66,8 +65,7 @@ if(!muterole){
   let rolereturn = message.guild.roles.find("name", "• Minemen •");
 
   setTimeout(function(){
-    tomute.removeRoles(tomute.roles);
-    tomute.addRole(rolereturn.id)
+    (tomute.removeRoles(tomute.roles)) , (tomute.addRole(rolereturn.id))
     
     let rolesreturnEmbed = new Discord.RichEmbed()
     .setDescription(`~${tomute}~ needs their roles back`)
