a6596284917c1e1b61ae096edf795e8a4def170b
Maksim Savich <mpsavich@icloud.com>
2019-07-22 23:57:08 -0400

    1.3.4

--- command/tempmute.js
+++ command/tempmute.js
@@ -52,7 +52,7 @@ if(!muterole){
           message.delete().catch(O_o=>{});
           muteChannel.send(muteEmbed);
 
-          await(tomute.removeRoles);
+          await(tomute.removeRoles());
           await(tomute.addRole(muterole.id));
 
       try{
