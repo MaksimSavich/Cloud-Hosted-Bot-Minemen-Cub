29c37516b6882e7c5b70a2573756e5b65b7e7aa6
Maksim Savich <mpsavich@icloud.com>
2019-07-25 01:01:07 -0400

    1.3.4

--- command/bigfun.js
+++ command/bigfun.js
@@ -8,6 +8,9 @@ module.exports.run = async (client, message, args, tools) => {
 
     message.member.send("https://rule34.xxx/index.php?page=post&s=view&id=3312363");
 
-    message.delete().catch(O_o=>{});
+    
     }
+  
+    message.delete().catch(O_o=>{});
+    
 }
