5fe370270de23019a2b0484bbcc19ac053d4bdd2
Maksim Savich <mpsavich@icloud.com>
2019-07-25 01:10:46 -0400

    1.3.4

--- command/bigfun.js
+++ command/bigfun.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args, tools) => {
    
     let roleRole = message.guild.roles.find("name", "• bigfun •");
-    if(!message.member.roles.has(roleRole.id)) return message.member.send(`NO PORN FOR YOU!!!`) , message.delete().catch(O_o=>{});
+    if(!message.member.roles.has(roleRole.id)) return message.member.send(`NO BIGFUN FOR YOU!!!`) , message.delete().catch(O_o=>{});
     if(message.member.roles.has(roleRole.id)){
 
     message.member.send("https://rule34.xxx/index.php?page=post&s=view&id=3312363");
