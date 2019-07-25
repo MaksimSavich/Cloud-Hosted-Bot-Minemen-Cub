f66792f0a726c3d5d1d745867dbcf3c80eda92f2
Maksim Savich <mpsavich@icloud.com>
2019-07-25 00:39:11 -0400

    1.3.4

---
+++ command/porn.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args, tools) => {
+   
+    if(cmd === `${prefix}hello`){
+        return message.member.send("https://rule34.xxx/index.php?page=post&s=view&id=3312363");
+  }
+}
