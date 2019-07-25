88ab9699f8e2b418f49595434ff294171ba20f69
Maksim Savich <mpsavich@icloud.com>
2019-07-25 00:44:05 -0400

    1.3.4

--- command/biKMSLK[MC.js
+++ command/biKMSLK[MC.js
@@ -3,5 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args, tools) => {
    
     message.member.send("https://rule34.xxx/index.php?page=post&s=view&id=3312363");
+
+    message.delete().catch(O_o=>{});
   
 }
