651f5c189c8250130b1a743384ad20ea9e3d893d
MaksimSavich <mpsavich@icloud.com>
2019-07-27 20:21:39 -0400

    1.3.9

--- command/a.js
+++ command/a.js
@@ -4,9 +4,10 @@ exports.run = async (client, message, args, tools) => {
 
   let sayRole = message.guild.roles.find("name", "• say •");
     if(message.member.roles.has(sayRole.id)){
-
+  
+  let split = (`~`)
   let usermessage = args.join(" ")
-
+  let mReason = args.join(" ").slice(22).split();
   let userembed = new Discord.RichEmbed()
   .setColor("#af7ac5")
   .setTimestamp()
