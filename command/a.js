edf592cd1aa9fb37035183933f291424e1c11bb7
MaksimSavich <mpsavich@icloud.com>
2019-07-27 20:26:34 -0400

    1.3.9

--- command/a.js
+++ command/a.js
@@ -7,12 +7,12 @@ exports.run = async (client, message, args, tools) => {
   
   let split = (`~`)
   let usermessage = args.join(" ")
-  let mReason = args.join(" ").slice(22).split();
+  let title = args.join(" ").slice(22).split();
   let userembed = new Discord.RichEmbed()
   .setColor("#af7ac5")
   .setTimestamp()
   .setFooter(`${message.author.tag}`, message.author.displayAvatarURL)
-  .addField('Announcement', (usermessage));
+  .addField((title), (usermessage));
 
     message.delete().catch(O_o=>{});
 
