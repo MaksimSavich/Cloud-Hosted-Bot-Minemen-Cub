14052cb68313a93d413e4c0edb1b4cc6b4ae8551
Maksim Savich <mpsavich@icloud.com>
2019-07-28 01:16:54 -0400

    1.3.9

---
+++ command/announce.js
@@ -0,0 +1,48 @@
+const send = require (`quick.hook`);
+const Discord = require ("discord.js");
+
+ 
+exports.run = async (client, message, args, tools) => {
+
+    let sayRole = message.guild.roles.find("name", "• say •");
+    if(message.member.roles.has(sayRole.id)){
+        
+        let split = `~`;
+
+        if (!args[0]) {
+
+            const embed = new Discord.RichEmbed()
+                .setColor("#af7ac5")
+                .setTitle("How to use the announcement command.")
+                .setDescription(`**Usage: \`\`\`^announce embedTitle ${split} embedMsg\`\`\`**`);
+
+                    return message.member.send(embed)
+    }
+
+    args = args.join(` `).split(split);
+
+        for (var i = 0; i < args.length; i++) args[i] = args[i].trim();
+
+             let options = {
+                title: args[0] || `Announcement`,
+                message: args[1] || undefined,
+                name: `Minemen Den | Official`,
+                 embedColor: `#af7ac5`
+    }
+
+    const embed = new Discord.RichEmbed()
+        .setColor(options.embedColor)
+        .setTitle(options.title)
+
+    if (options.message) embed.setDescription(options.message);
+
+        return message.channel.send(embed)
+
+        
+    }
+}
+
+module.exports.help = {
+    name: "announce",
+    aliases: ["a", "A", "Announce", "announcement", "Announcement"]
+  }
