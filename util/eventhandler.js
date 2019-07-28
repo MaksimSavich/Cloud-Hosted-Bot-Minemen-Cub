5ef6f49f115abf057fdca8ab5004c1eb6002fb18
Maksim Savich <mpsavich@icloud.com>
2019-07-28 18:58:59 -0400

    1.4.0

--- util/eventhandler.js
+++ util/eventhandler.js
@@ -4,6 +4,6 @@ module.exports = bot => {
     client.on("ready", function() {reqEvent("ready") (client)});
     client.on("reconnecting", () => reqEvent("reconnecting") (client))
     client.on("disconnect", () => reqEvent("disconnect") (client))
-    client.on("warn", reqEvent("warn") (client))
-    client.on("error", reqEvent("error") (client))
+    client.on("warn", reqEvent("warn") )
+    client.on("error", reqEvent("error") )
 }
