96a4af9eec60777b318d9092ac3cc41fd53dd509
Maksim Savich <mpsavich@icloud.com>
2019-07-28 03:57:44 -0400

    1.4.0

--- command/tempmute.js
+++ command/tempmute.js
@@ -3,7 +3,7 @@ const ms = require(`ms`);
 
 exports.run = async (client, message, args, tools) => {
 
-let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args [0]));
+let tomute = message.guild.member(message.mentions.users.first());
 if(!tomute) return message.member.send(`Can't find user!`) , message.delete().catch(O_o=>{});
 if(tomute.hasPermission(`MANAGE_MESSAGES`)) return message.member.send(`You can't mute this person.`) , message.delete().catch(O_o=>{});
 let muterole = message.guild.roles.find(`name`, "• suspended •");
@@ -32,8 +32,8 @@ if(!muterole){
   let roles = message.guild.member(message.mentions.users.first()).roles.map(role => role.name).join(", ");
   let split = "~"
   args = args.join(" ").split(split);
-  let mutetime = (args[1]);
-  let mReason = (args[2]);
+  let mutetime = (args[0]);
+  let mReason = (args[1]);
 
   if(!mutetime) return message.member.send(`You must specify a time!`) , message.delete().catch(O_o=>{});
 
