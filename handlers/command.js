8bc49f352ec0e5c447170fbe6e66b4dd8f1a9e8c
Maksim Savich <mpsavich@icloud.com>
2019-07-28 19:53:24 -0400

    1.4.0

--- handlers/command.js
+++ handlers/command.js
@@ -4,6 +4,7 @@ module.exports = (client) => {
     const load = dirs => {
         const commands  = readdirSync(`./command/${dirs}/`).filter(d => d.endsWith(`.js`))
         for  (let file of commands) {
+            client.commands = new Discord.Collection();
             const pull = require(`../command/${dirs}/${file}`)
         client.commands.set(pull.config.name, pull)
         if(pull.config.aliases) pull.config.aliases.forEach(a => client.aliases.set(a, pull.config.name))
