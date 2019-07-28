872010b904e869f34a208f505cc3696ac41ecdc2
Maksim Savich <mpsavich@icloud.com>
2019-07-28 03:50:52 -0400

    1.4.0

--- command/tempmute.js
+++ command/tempmute.js
@@ -30,10 +30,10 @@ if(!muterole){
   }
 
   let roles = message.guild.member(message.mentions.users.first()).roles.map(role => role.name).join(", ");
-  let split = "-"
+  let split = "~"
   args = args.join(" ").split(split);
-  let mutetime = args[1];
-  let mReason = args[2];
+  let mutetime = (args[0]);
+  let mReason = (args[1]);
 
   if(!mutetime) return message.member.send(`You must specify a time!`) , message.delete().catch(O_o=>{});
 
