b502d60e00541ca024218af6a3cc749812a42431
Maksim Savich <mpsavich@icloud.com>
2019-07-28 19:20:55 -0400

    1.4.0

--- handlers/command.js
+++ handlers/command.js
@@ -9,5 +9,5 @@ module.exports = (client) => {
         if(pull.config.aliases) pull.config.aliases.forEach(a => clientInformation.aliases.set(a, pull.config.name))
         }
     }
-    ["member" , "admin" , "staff" , "moderator"].forEach(x => load(x))
+    ["admin" , "staff" , "moderator"].forEach(x => load(x))
 }
