d6a88be7314bd389a02e345732c27537623bf29c
Maksim Savich <mpsavich@icloud.com>
2019-07-28 19:39:51 -0400

    1.4.0

--- handlers/command.js
+++ handlers/command.js
@@ -2,9 +2,9 @@ const { readdirSync } = require("fs");
 
 module.exports = (client) => {
     const load = dirs => {
-        const commands  = readdirSync(`.command/${dirs}/`).filter(d => d.endsWith(`.js`))
+        const commands  = readdirSync(`..command/${dirs}/`).filter(d => d.endsWith(`.js`))
         for  (let file of commands) {
-            const pull = require(`./command/${dirs}/${file}`)
+            const pull = require(`../command/${dirs}/${file}`)
         client.commands.set(pull.config.name, pull)
         if(pull.config.aliases) pull.config.aliases.forEach(a => client.aliases.set(a, pull.config.name))
         }
