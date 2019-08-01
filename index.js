b5ca2f8dbf791eece2176d3d20d0863fd2d33f24
Maksim Savich <mpsavich@icloud.com>
2019-08-01 15:56:14 -0400

    1.4.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const fs = require(`fs`);
 const modules = ['administration'];
 
-name = new Discord.Client();
+
 client.commands = new Discord.Collection(); // Collection for all commands
 client.aliases = new Discord.Collection(); // Collection for all aliases of every command
 
@@ -16,6 +16,7 @@ modules.forEach(c => {
   
   files.forEach(f => {
   const props = require(`./command/${c}/${f}`);
+  name = new Discord.Client();
   client.commands.set(props.help.name, props);
   props.conf.aliases.forEach(alias => {
   client.aliases.set(alias, props.name);
