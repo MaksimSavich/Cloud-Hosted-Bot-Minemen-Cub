cf13b3a7e1ad06e337f590f093be8876cd576675
Maksim Savich <mpsavich@icloud.com>
2019-08-01 19:17:39 -0400

    1.4.0

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ fs.readdir(`./command/${c}/`, (err, files) => { // Here we go through all folder
 if (err) throw err; // If there is error, throw an error in the console
 console.log(`[Commandlogs] Loaded ${files.length} commands of module ${c}`); // When commands of a module are successfully loaded, you can see it in the console
 files.forEach(f => { // Now we go through all files of a folder (module)
-const props = require(`./command/${c}/${f}`); // Location of the current command file
+const props = require(`./command/${c}/${f}/`); // Location of the current command file
 client.commands.set(props.help.name, props); // Now we add the commmand in the client.commands Collection which we defined in previous code
-})
 
 props.conf.aliases.forEach(alias => { // It could be that the command has aliases, so we go through them too
 client.aliases.set(alias, props.name); // If we find one, we add it to the client.aliases Collection
       
-      
+});
     });
   });
 });
