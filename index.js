97eabe2eeea2f4bc4085ddf0e4c27395e0c4713b
Maksim Savich <mpsavich@icloud.com>
2019-08-01 15:03:40 -0400

    1.4.0

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
-client.commands = new Enmap();
+client.commands = new enmap();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
