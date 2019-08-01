8d8c87eca3b8d253a7a3e7129f32521b9e9dc96a
Maksim Savich <mpsavich@icloud.com>
2019-08-01 15:12:19 -0400

    1.4.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const Enmap = require("enmap");
 const client = new Discord.Client();
 const fs = require(`fs`);
 const supereagent = require("superagent");
