351d76bdb402bae1f17b4d423579c86d6f032d1e
Maksim Savich <mpsavich@icloud.com>
2019-08-01 14:51:03 -0400

    1.4.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require(`fs`);
 const supereagent = require("superagent");
 const prefix = `^`;
 
-const config = require("./config.json");
+const config = require("./botconfig.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
