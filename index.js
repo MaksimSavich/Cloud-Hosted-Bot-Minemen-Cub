e412039f4fa5841d902ecd65cbf77d86585a788e
Maksim Savich <mpsavich@icloud.com>
2019-08-01 16:52:12 -0400

    1.4.0

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ fs.readdir("./events/", (err, files) => {
 
 
 client.aliases = new Discord.Collection();
+const client = new Discord.Client();
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
@@ -30,13 +44,8 @@ fs.readdir("./commands/", (err, files) => {
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.name);
-    });
   });
 });
-
-
 
 
 
