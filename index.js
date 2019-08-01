404caeef5c50a21a12610feb6e314c6a711de775
Maksim Savich <mpsavich@icloud.com>
2019-08-01 17:25:20 -0400

    1.4.0

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const supereagent = require("superagent");
 const prefix = `^`;  
 
 const config = require("./botconfig.json");
-client.aliases = new Discord.Collection();
+
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
@@ -22,7 +22,7 @@ fs.readdir("./events/", (err, files) => {
 });
 
 client.commands = new Enmap();
-
+client.aliases = new Discord.Collection();
 fs.readdir("./command/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
