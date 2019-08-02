0502c5a0ccbcc9737044dbc620a9931c2050c170
Maksim Savich <mpsavich@icloud.com>
2019-08-01 20:08:19 -0400

    1.4.0

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const Enmap = require("enmap");
 require('discord.js-aliases');
 
 client.commands = new Discord.Collection(); // Collection for all commands
 client.aliases = new Discord.Collection(); // Collection for all aliases of every command
-const command = ["administration" , "test"]; // This will be the list of the names of all modules (folder) your bot owns
+const modules = ["administration" , "test"]; // This will be the list of the names of all modules (folder) your bot owns
 const fs = require('fs'); // Require fs to go throw all folder and files
 
-command.forEach(c => {
-fs.readdir(`./command/${c}/`, (err, files) => { // Here we go through all folders (modules)
-if (err) throw err; // If there is error, throw an error in the console
-console.log(`[Commandlogs] Loaded ${files.length} commands of module ${c}`); // When commands of a module are successfully loaded, you can see it in the console
 
-files.forEach(f => { // Now we go through all files of a folder (module)
-const props = require(`./command/${c}/${f}/`); // Location of the current command file
-client.commands.set(props.help.name, props); // Now we add the commmand in the client.commands Collection which we defined in previous code
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+
 
 props.conf.aliases.forEach(alias => { // It could be that the command has aliases, so we go through them too
 client.aliases.set(alias, props.name); // If we find one, we add it to the client.aliases Collection
@@ -23,7 +37,7 @@ client.aliases.set(alias, props.name); // If we find one, we add it to the clien
       });
     });
   });
-});
+
 
 
 
