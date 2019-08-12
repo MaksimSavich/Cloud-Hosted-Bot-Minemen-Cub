acc2cd86f6b0e4e0ec2a15c56fde5f8ca6d5a96f
Maksim Savich <mpsavich@icloud.com>
2019-08-11 20:41:16 -0400

    1.4.0

--- command/ping.js
+++ command/ping.js
@@ -1,17 +1,17 @@
 module.exports.run = (client, message, args, tools) => {
 
-  message.channel.send({embed: {
-    color: 0x2ed32e,
-    fields: [{
-        name: "Pong",
-        value: "My Ping: " + Math.round(client.ping) + ' ms'
-  }
- ]
 
+  const embed = new Discord.RichEmbed()
+    .setTitle("Member")
+    .setAuthor("Minemen Cub | Ping", client.user.avatarURL)
+    
+    .setColor("#af7ac5")
+    .addField("Bot Ping: " , Math.round(client.ping) + ' ms')
 
-    }
-  })
+    message.channel.send({embed});
+
 }
+
 module.exports.config = {
   name: "ping",
   aliases: ["p"]
