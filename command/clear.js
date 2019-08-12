d8be8c51ce1ca31200c2e3cd34a5cfdf37f2e3d1
Maksim Savich <mpsavich@icloud.com>
2019-08-11 21:16:27 -0400

    1.4.0

--- command/clear.js
+++ command/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args, tools) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't perform this action!");
-  if(!args[0]) return message.channel.send("You can't perform this action!");
+  if(!args[0]) return message.channel.send("You must specify a number!");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(1500));
   })
