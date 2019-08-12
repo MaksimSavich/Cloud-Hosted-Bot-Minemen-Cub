27891b8e5e111dfd17aa4452d4450cad4f812385
Maksim Savich <mpsavich@icloud.com>
2019-08-11 23:38:56 -0400

    1.4.0

--- index.js
+++ index.js
@@ -138,14 +138,6 @@ client.on("ready", async () => {
 });
 
 
-app.get("/ip", (req, res) => {
-  console.log(req.ip) // "::ffff:127.0.0.1" ::ffff: is a subnet prefix for IPv4 (32 bit) 
-  let ip = req.ip.split(':');
-  console.log(ip[3]);
-   res.json(ip[3]);  // ==> 127.0.0.1 You can Get Your Ip address only 
-});
-app.listen(3003);
-
     //Auto Welcome
 
   client.on('guildMemberAdd', member => {
